678c377b52276bc57ed080f29878d4015395e45d
Lorenzo <scianaticolorenzo@gmail.com>
2020-03-16 20:10:16 +0100

    Initial commit

---
+++ src/test/java/com/lorescianatico/patterns/PatternsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lorescianatico.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
