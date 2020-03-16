6cf158e201d1fb92d16318dcf3123cc4bc1f45e6
Lorenzo <scianaticolorenzo@gmail.com>
2020-03-16 20:10:46 +0100

    Initial commit

--- src/test/java/com/lorescianatico/patterns/PatternsApplicationTests.java
+++ src/test/java/com/lorescianatico/patterns/PatternsApplicationTests.java
@@ -3,8 +3,6 @@ package com.lorescianatico.patterns;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.net.URI;
-
 @SpringBootTest
 class PatternsApplicationTests {
 
