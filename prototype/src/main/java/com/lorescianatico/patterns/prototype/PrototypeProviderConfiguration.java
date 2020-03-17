43d7878c2fac1c1c28cdbda17c770e64abb586de
Lorenzo <scianaticolorenzo@gmail.com>
2020-03-17 13:02:22 +0100

    Adding configuration for running in spring context

---
+++ prototype/src/main/java/com/lorescianatico/patterns/prototype/PrototypeProviderConfiguration.java
@@ -0,0 +1,9 @@
+package com.lorescianatico.patterns.prototype;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class PrototypeProviderConfiguration {
+}
